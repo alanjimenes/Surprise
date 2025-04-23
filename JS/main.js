d0a9c427c8c4938228585320fdb37656635c00da
Alan Jimenez <64500750+alanjimenes@users.noreply.github.com>
2025-04-23 15:31:07 -0400

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -18,3 +18,24 @@ $(document).ready(function () {
 function closeCard() {
   $('.card-inner').slideUp('slow');
 }
+
+let estadoCarta = 0;
+
+$(document).ready(function () {
+   $('.card').click(function () {
+      if (estadoCarta === 0) {
+         $(this).animate({
+            top: '-90px'
+         }, 'slow');
+         estadoCarta = 1;
+      } else if (estadoCarta === 1) {
+         $('.card-inner').slideDown('slow');
+         estadoCarta = 2;
+      }
+   });
+});
+
+function closeCard() {
+   $('.card-inner').slideUp('slow');
+   estadoCarta = 1; // vuelve a permitir ver la carta de nuevo si se cierra
+}
